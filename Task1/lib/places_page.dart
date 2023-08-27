f7ab887a6bb60c1fe2c1428ddb2477dc95888437
jay vashishtha <jayvashishtha@jays-MacBook-Air.local>
2023-08-27 09:40:09 +0530

    first commit

---
+++ Task1/lib/places_page.dart
@@ -0,0 +1,109 @@
+List places = [
+  {
+    "name": "TAJ MAHAL",
+    "horname": 'Shimla',
+    "horlocation": 'Shimla City',
+    "horimg": "assets/images/shimla.jpeg",
+    "img": "assets/images/tajmahal.jpg",
+    "preimg": "assets/images/Hill_station.jpeg",
+    "rating": "4.9",
+    "place": "Mountains",
+    "icon": "assets/images/Mountain_icon.png",
+    "disc":
+        "Jay abjhhdhs dhfuh  akoifi skksofw haihywdw jay avrhda gdhiajdjdhfd ",
+    "price": r"$10/Person",
+    "horprice": "\$150\n/Person",
+    "location": "Agra, India",
+    "details": "It is a historical Place in the neighbourhood of Delhi, India."
+        "That historically served as the main residence of the Mughal emperors."
+        "Emperor Shah Jahan commissioned construction of the Red Fort on 12th May 1638."
+        "when he decided to shift his capital from Agra to Delhi. Originally red and white."
+        "Its design is credited to architect Ustad Ahmad Lahori, who also constructed the Taj Mahal."
+        "The fort represents the peak in Mughal architecture under Shah Jahan and combines Persianate palace architecture with Indian traditions.",
+  },
+  {
+    "name": "Red Fort",
+    "horname": 'Historical places Tour',
+    "horlocation": 'Delhi, India',
+    "horimg": "assets/images/historical_places.jpeg",
+    "img": "assets/images/Red_fort.jpg",
+    "preimg": "assets/images/Red_fort.jpg",
+    "rating": "4.7",
+    "place": "Waterfall",
+    "icon": "assets/images/waterfall.png",
+    "disc": "One of the oldest places of india. ",
+    "price": r"$20/Person",
+    "horprice": "\$80\n/Person",
+    "location": "Old Delhi, India",
+    "details": "The Red For or lal-Qila is a historic fort in Delhi neighbourhood of Delhi, India."
+        "That historically served as the main residence of the Mughal emperors."
+        "Emperor Shah Jahan commissioned construction of the Red Fort on 12th May 1638."
+        "when he decided to shift his capital from Agra to Delhi. Originally red and white."
+        "Its design is credited to architect Ustad Ahmad Lahori, who also constructed the Taj Mahal."
+        "The fort represents the peak in Mughal architecture under Shah Jahan and combines Persianate palace architecture with Indian traditions.",
+  },
+  {
+    "name": "Lotus Temple",
+    "horname": 'Disney land',
+    "horlocation": 'Florida, USA',
+    "horimg": "assets/images/Disney_land.webp",
+    "img": "assets/images/lotus-temple.jpeg",
+    "preimg": "assets/images/newdelhi.jpg",
+    "rating": "3.2",
+    "place": "River",
+    "icon": "assets/images/river.png",
+    "disc": "Lotus Temple Is a Shape of Lotus.",
+    "price": r"$90/Person",
+    "horprice": "\$80\n/Person",
+    "location": "New Delhi, India",
+    "details": "The Lotus Temple, located in New Delhi, India, is a Baháʼí House of Worship that was dedicated in December 1986."
+        " Notable for its lotus-like shape, it has become a prominent attraction in the city."
+        " Like all Bahá’í Houses of Worship, the Lotus Temple is open to all, regardless of religion or any other qualification."
+        "The building is composed of 27 free-standing marble-clad  arranged in clusters of three to form nine sides."
+        "With nine doors opening onto a central hall with a height of slightly over 34 meters[1] and a capacity of 1,300 people."
+        "[2] The Lotus Temple has won numerous architectural awards[3][4] and has been featured in many newspaper and magazine articles.[5]",
+  },
+  {
+    "name": "Eiffel Tower",
+    "horname": 'Rinjani Mountain',
+    "horlocation": 'Lombok, Indonesia',
+    "horimg": "assets/images/KAGAGAHAN_RIJANI.jpg",
+    "img": "assets/images/Eiffel_tower.jpeg",
+    "preimg": "assets/images/temple.jpg",
+    "rating": "4.9",
+    "place": "Desert",
+    "icon": "assets/images/desert.png",
+    "disc": "Eiffel Tower ^Lombok Ind.",
+    "price": r"$100/Person",
+    "horprice": "\$65\n/Person",
+    "location": "Paris, France",
+    "details": "Locally nicknamed La dame de fer (French for Iron Lady), it was constructed from 1887 to 1889 as the centerpiece of the 1889 World's Fair."
+        "Although initially criticised by some of France leading artists and intellectuals for its design."
+        "it has since become a global cultural icon of France and one of the most recognisable structures in the world."
+        "The tower received 5,889,000 visitors in 2022, up by 197 per cent from 2021, when numbers dropped due to the COVID virus."
+        "The Eiffel Tower is the most visited monument with an entrance fee in the world:[5] 6.91 million people ascended it in 2015."
+        "It was designated a monument historique in 1964, and was named part of a UNESCO World Heritage Site (Paris, Banks of the Seine) in 1991.[6]",
+  },
+  {
+    "name": "The Great wall Of China",
+    "horname": 'Las-vegas',
+    "horlocation": 'Nevada,USA',
+    "horimg": "assets/images/las_vegas.jpeg",
+    "img": "assets/images/china_wall.webp",
+    "preimg": "assets/images/las.jpeg",
+    "rating": "4.0",
+    "place": "Town",
+    "icon": "assets/images/city.png",
+    "disc": "China's powerfull wall 6000km long. ",
+    "price": r"$200/Person",
+    "horprice": "\$25\n/Person",
+    "location": "Hebei Province, China",
+    "details": "The city bills itself as the Entertainment Capital of the World, and is famous for its luxurious and extremely large casino-hotels."
+        "With over 2.9 million visitors as of 2019, Las Vegas is the sixth-most visited city in the U.S."
+        "After New York City, Miami, Los Angeles, Orlando, and San Francisco.[8] It is a top-three destination in the U.S. for business conventions"
+        "and a global leader in the hospitality industry, claiming more AAA Five Diamond hotels than any other city in the world."
+        "[9][10][11] Las Vegas annually ranks as one of the world's most visited tourist destinations.[12][13]"
+        "The city's tolerance for numerous forms of adult entertainment earned it the nickname "
+        "and has made Las Vegas a popular setting for literature, films, television programs, and music videos.",
+  },
+];
